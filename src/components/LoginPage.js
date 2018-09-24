a1dee4eff88a431868ade389939fefd7adc1f216
HackerRank <hackerrank@example.com>
2018-09-24 18:34:01 +0000

    Saved at Mon Sep 24 2018 19:34:01 GMT+0100 (British Summer Time)

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import ax
+import axios f
 
 import { userActions } from '../actions';
 
