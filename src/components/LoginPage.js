ba69755ab19875d710a23cb3d5259fe7980b9e65
HackerRank <hackerrank@example.com>
2018-09-24 18:49:47 +0000

    Saved at Mon Sep 24 2018 19:49:47 GMT+0100 (British Summer Time)

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -30,7 +30,7 @@ class LoginPage extends Component {
     }
 
     handleSubmit(e) {
-        userActions.login(this.state.username, this.state.passw)
+        userActions.login(this.state.username, this.state.password)
         
     }
 
