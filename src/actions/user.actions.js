7a4db050861f60571f0ce1047a19f4c8c92771b1
HackerRank <hackerrank@example.com>
2018-09-24 18:53:39 +0000

    Saved at Mon Sep 24 2018 19:53:38 GMT+0100 (British Summer Time)

--- src/actions/user.actions.js
+++ src/actions/user.actions.js
@@ -12,9 +12,9 @@ export const userActions = {
 function login(username, password) {
     // return the promise using fetch which adds to localstorage on resolve
     retrun new Promise(() => {
-        return 'a';
+        return 'a'
     }, () => {
-        return 
+        return 'b'
     })
 
     function request(user) { return { type: userConstants.LOGIN_REQUEST, user } }
@@ -27,7 +27,7 @@ function logout() {
 }
 
 function register(user) {
-    // return the promise using fetch which dispatches appropriately 
+    // return the promise using fetch which dispatches appropriately
 
     function request(user) { return { type: userConstants.REGISTER_REQUEST, user } }
     function success(user) { return { type: userConstants.REGISTER_SUCCESS, user } }
