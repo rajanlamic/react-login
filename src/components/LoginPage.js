6919a435bdd6a4adf767373da50bf0d97bd195a0
HackerRank <hackerrank@example.com>
2018-09-24 18:32:48 +0000

    Saved at Mon Sep 24 2018 19:32:48 GMT+0100 (British Summer Time)

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -49,7 +49,7 @@ class LoginPage extends Component {
                         }
                     </div>
                     <div className="form-group">
-                        <button className="btn btn-primary" onCl>Login</button>
+                        <button className="btn btn-primary" onClick={>Login</button>
                     </div>
                 </form>
             </div>
