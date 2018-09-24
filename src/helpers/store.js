03935a9dc35f6eca7dc98adbb38f29e2b2810ed7
HackerRank <hackerrank@example.com>
2018-09-24 18:58:02 +0000

    Saved at Mon Sep 24 2018 19:58:01 GMT+0100 (British Summer Time)

--- src/helpers/store.js
+++ src/helpers/store.js
@@ -3,7 +3,25 @@ import thunkMiddleware from 'redux-thunk';
 import rootReducer from '../reducers';
 
 
+const composeEnhancers =
+    typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+      ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+          // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+        })
+      : "";
 
+  const enhancer = composeEnhancers(
+    applyMiddleware(logger, thunk, promiseMiddleware())
+  );
+
+ export const store = createStore(
+    reducers,
+    /* preloadedState, */ composeEnhancers(
+      applyMiddleware(thunk, promiseMiddleware(), sagaMiddleware)
+      // other store enhancers if any
+    )
+  );
+  
 export const store = createStore(
     rootReducer,
     applyMiddleware(thunkMiddleware)
