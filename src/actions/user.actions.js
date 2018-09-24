c70c25669fdd0da4fb966be17f68c4256585c280
HackerRank <hackerrank@example.com>
2018-09-24 19:08:44 +0000

    Saved at Mon Sep 24 2018 20:08:44 GMT+0100 (British Summer Time)

--- src/actions/user.actions.js
+++ src/actions/user.actions.js
@@ -11,7 +11,7 @@ export const userActions = {
 
 function login(username, password) {
     // return the promise using fetch which adds to localstorage on resolve
-    return new Promise((resolve, reject) = > {
+    return new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve('aa')
         }, 1000)
